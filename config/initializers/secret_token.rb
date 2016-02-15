514bdb841c98474bb56713976d516f71a94ef9cb
Petrov.d <petrov.daniil92@gmail.com>
2016-02-15 16:51:04 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Billboard::Application.config.secret_key_base = 'd8c3505bb4015d2d41b4693182aaa61cbdfd6905c7204db8dddb446561c7aa92f44443a81824cf96d8dae00b93b2aa3015be33b8fd34588321dd5c1b82a341d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Billboard::Application.config.session_store :cookie_store, key: '_billboard_session'
