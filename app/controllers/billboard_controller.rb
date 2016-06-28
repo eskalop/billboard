9d332640f4b7e53ece824db83fcc725162b6fea2
Petrov.d <petrov.daniil92@gmail.com>
2016-06-28 23:12:43 +0300

    added main page and part of application layouts

---
+++ app/controllers/billboard_controller.rb
@@ -0,0 +1,5 @@
+class BillboardController < ApplicationController
+  def index
+    #  @ads = Ad.order(:user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Billboard::Application.routes.draw do
+  get "billboard/index"
+
+
+
+
+
+  root to: 'billboard#index', as: 'billboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +46,7 @@ Billboard::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
